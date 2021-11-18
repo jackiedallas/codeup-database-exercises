d1bc98e742df4a79deb3da72b7803130a879fa4f
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 10:59:52 -0600

    completed update sales and release date

--- update_exercises.sql
+++ update_exercises.sql
@@ -13,3 +13,12 @@ WHERE release_date < 1980;
 SELECT name AS 'Michael Jackson Albums'
 FROM albums
 WHERE artist = 'Michael Jackson';
+
+# make all albums 10 times more popular
+UPDATE albums
+SET sales = sales * 10;
+
+# move all albums before 1980 back to the 1800s
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date <= 1980;
