ab290473f15f2f31ca192777693743dc1eb637e9
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 10:52:30 -0600

    completed all albums before 1990

--- select_exercises.sql
+++ select_exercises.sql
@@ -6,7 +6,7 @@ FROM albums
 WHERE artist = 'Pink Floyd';
 
 # Year Sgt. Pepper's Lonely Hearts Club Band was released.
-SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club was released in'
+SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club was released in', release_date
 FROM albums
 WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
@@ -16,17 +16,17 @@ FROM albums
 WHERE name = 'Nevermind';
 
 # Albums released in 1990s
-SELECT name AS 'Albums released in the 90s'
+SELECT name AS 'Albums released in the 90s', release_date
 FROM albums
 WHERE release_date BETWEEN 1989 AND 1991;
 
 # Albums with less than 20 million in sales
-SELECT name as 'Albums with less than 20 million copies sold'
+SELECT name as 'Albums with less than 20 million copies sold', sales
 FROM albums
 WHERE sales < 20;
 
 # Albums with the genre of rock
-SELECT name as '''Rock'' albums'
+SELECT name as '''Rock'' albums', genre
 FROM albums
 WHERE genre = 'rock';
 
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,5 +1,10 @@
 USE codeup_test_db;
 
-# all albums
-SELECT name AS 'All Albums'
+# output all albums
+SELECT name AS 'All Albums', artist, release_date
 FROM albums;
+
+# output all albums before 1990
+SELECT name AS 'All Albums before 1990', artist, release_date
+FROM albums
+WHERE release_date < 1990;
