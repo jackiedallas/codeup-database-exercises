cd84cb9fba2f38856403425e1e3d5dfa70313a8a
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 11:02:47 -0600

    finished updates

--- update_exercises.sql
+++ update_exercises.sql
@@ -22,3 +22,7 @@ SET sales = sales * 10;
 UPDATE albums
 SET release_date = release_date - 100
 WHERE release_date <= 1980;
+
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
