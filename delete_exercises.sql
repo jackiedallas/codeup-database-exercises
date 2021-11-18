a372698207ebfba1c0b3eeeb05aa81cae132fe44
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 11:24:14 -0600

    refactoring delete exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,13 +1,17 @@
 USE codeup_test_db;
 
+# albums after 91'
 SELECT *
 FROM albums
 WHERE release_date > 1991;
 
+# albums with the genre 'disco'
 SELECT *
 FROM albums
-WHERE genre LIKE '%disco%';
+WHERE genre = 'disco';
 
+# albums by the beatles
 SELECT *
 FROM albums
-WHERE artist = 'Whitney Houston';
+WHERE artist = 'The Beatles';
+
