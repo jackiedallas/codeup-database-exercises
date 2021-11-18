766373f38a09bee9d7feb9de0c6b9fc646c1c344
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 16:36:20 -0600

    completed offset limit

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,4 +4,8 @@ SELECT DISTINCT title FROM titles;
 
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
-SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 49;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
+
+# SELECT emp_no FROM employees ORDER BY emp_no;
+
+# SELECT emp_no FROM employees ORDER BY emp_no LIMIT 5 OFFSET 45;
