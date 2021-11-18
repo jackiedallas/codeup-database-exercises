850902b814694d100421c157ea39a0386092b405
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 09:49:49 -0600

    finished adding albums to album table

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,12 +2,12 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE albums (
+CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) DEFAULT 'NONE',
     name VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
-    sales FLOAT (11, 2) NOT NULL,
-    genre VARCHAR(20),
+    release_date INT,
+    sales FLOAT,
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
