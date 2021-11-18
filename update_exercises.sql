1fcd4d57e17792bf8eda7b37446dedd218690184
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 10:54:52 -0600

    completed select all michael jackson albums

--- update_exercises.sql
+++ update_exercises.sql
@@ -5,6 +5,11 @@ SELECT name AS 'All Albums', artist, release_date
 FROM albums;
 
 # output all albums before 1990
-SELECT name AS 'All Albums before 1990', artist, release_date
+SELECT name AS 'All Albums before 1980', artist, release_date
 FROM albums
-WHERE release_date < 1990;
+WHERE release_date < 1980;
+
+# albums by michael jackson
+SELECT name AS 'Michael Jackson Albums'
+FROM albums
+WHERE artist = 'Michael Jackson';
