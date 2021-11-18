e51734afa2bf1645701f7ffee381f043a707aa32
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-18 10:22:28 -0600

    completed genres for nevermind

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,12 +1,17 @@
 USE codeup_test_db;
 
 # Name all Pink Floyd albums.
-SELECT name AS 'Pink Floyd Albums'
+SELECT name AS 'The name of all Pink Floyd Albums'
 FROM albums
 WHERE artist = 'Pink Floyd';
 
 # Year Sgt. Pepper's Lonely Hearts Club Band was released.
-SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club was released'
+SELECT release_date AS 'Sgt. Pepper''s Lonely Hearts Club was released in'
 FROM albums
 WHERE artist = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+# Genre for Nevermind
+SELECT genre AS 'Nevermind''s genre is'
+FROM albums
+WHERE artist = 'Nevermind';
 
