5d09729dd4b561a021217495cc7c7137a071c45d
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-19 13:55:28 -0600

    completed number 3

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -3,3 +3,16 @@ USE employees;
 SELECT DISTINCT title AS 'Employee Titles'
 FROM titles
 GROUP BY title;
+
+SELECT last_name AS 'Last Names that Start and End with E'
+FROM employees
+# GROUP BY last_name LIKE 'e%', last_name LIKE '%e'
+# ORDER BY last_name;
+
+# GROUP BY last_name LIKE 'e%'
+# AND last_name LIKE '%e';
+WHERE last_name LIKE 'e%'
+AND last_name LIKE '%e'
+GROUP BY last_name ORDER BY last_name;
+
+
