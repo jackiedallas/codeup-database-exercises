4e35bc79ba1723167a97211046e7b4f27d634025
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-23 13:44:09 -0600

    starting db design lecture

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,3 +2,10 @@ USE employees;
 
 SELECT * FROM dept_manager WHERE to_date > NOW();
 
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE emp_no IN (
+ SELECT emp_no
+ FROM employees
+ WHERE hire_date = '1990-10-22'
+);
